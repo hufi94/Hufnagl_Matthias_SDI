7190974cb51c0e80e142d6ad52596a4184ed473b
hufi94 <dj_hufi@me.com>
2015-03-05 16:05:14 -0500

    Math class
    talking about odd and even

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -38,7 +38,52 @@ console.log(whatever1)
 
 var a = 2;
 
-console.log(a)
+console.log(a);
+
+var b;
 
 a=a+3;
-console.log(a)
+console.log(a);
+
+//define this var using an existing variable
+
+b= a+3;
+console.log(a);
+console.log(b);
+
+//simple math
+//find our ages
+
+//declare and define the year we were born
+
+var yearBorn =1994;
+
+//2015 minus the year we were born
+
+var ourAge = 2015-yearBorn;
+console.log(ourAge);
+//Talk about math
+//+,-,*,/
+//find the area of a triangle
+//base times height/2
+var base = 8;
+var height = 5;
+
+var areaTiangle= base*height/2;
+
+console.log(areaTiangle)
+
+//Modulo - %
+//Gives the reminder
+
+var decemal = 32/10;
+console.log(decemal);
+
+var reminder =32%10;
+console.log(reminder);
+
+//find out if even or odd
+// %1=odd 0 =even
+
+ var evenorodd = 67%2;
+console.log(evenorodd);
