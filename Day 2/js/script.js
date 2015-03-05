047db29f649e93bc5bde0c68e8f3e73a12f632e7
hufi94 <dj_hufi@me.com>
2015-03-05 14:54:28 -0500

    Day 2 intro
    js script

---
+++ Day 2/js/script.js
@@ -0,0 +1,19 @@
+/*
+    Matthias Hufnagl
+    o3/05.2015
+    Section 00
+    Day 2 intro
+ */
+
+//alert("testing to see if connected");
+
+/*
+Example of a mulilined comment.
+anything indside of here will not be seen by the computer
+ */
+
+//Single line comment. Only this line will be ignored.
+
+// Alert box- pop up that will alert the user with information
+
+alert("Text that the user will see");
