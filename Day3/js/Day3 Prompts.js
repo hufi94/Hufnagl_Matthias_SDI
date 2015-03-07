a33a27a6f0aa764240e54aeb170f2cd40bb2efdf
hufi94 <dj_hufi@me.com>
2015-03-07 15:27:41 -0500

    Learn To prompt
    Lets be spesific

---
+++ Day3/js/Day3 Prompts.js
@@ -0,0 +1,36 @@
+/* Matthias Hufnagl
+ 3-7-15
+ section 00
+ Day 3 Prompts
+ */
+
+//alert("Just Testing");
+
+//Ask the user for input
+// prompt("Text that the user will see");
+
+var userInput = prompt("Enter Year of Birth");
+console.log(userInput);
+
+// calculate the area of a rectangle
+// langth times width
+
+// lets prompt the user  for the width and height
+
+// lets explain to them what is going on.
+
+var height = prompt("Let's calculate the area of a rectangle! \nPlease  Enter the height of the rectangle:");
+
+var width = prompt("Please Enter the width of the rectangel:");
+
+//Calculate the ares of the rectangle
+
+var area = width * height;
+
+//tell the user the answer
+
+console.log("The area of the rectangle with a height of "+height+" and a width of "+width +" is "+area+".");
+
+var results ="The results of the area are "+ area;
+
+console.log(results);
