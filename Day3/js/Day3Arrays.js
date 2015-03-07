c3e16b74bb1ee9e9afea277deee7cc047a2ba5bf
hufi94 <dj_hufi@me.com>
2015-03-07 14:43:09 -0500

    Learn Basic Arrays
    Basic of arrays the length property
    advantures

--- Day3/js/Day3Arrays.js
+++ Day3/js/Day3Arrays.js
@@ -6,3 +6,55 @@
 
 //alert("Testing to make sure it linked");
 
+//create a basic array
+var adventuresNames = ["Thor", "Hulk", "Iron Man", "Captain America"];
+
+//Print out whole array
+
+console.log(adventuresNames);
+
+//Print out one spesific array
+
+console.log(adventuresNames[2]);
+
+//swap items in our array
+
+adventuresNames[1]="Black Widow";
+
+console.log(adventuresNames);
+
+//use a var for the index numb
+
+var num=3;
+
+console.log(adventuresNames[num]);
+
+//find the langth of an array
+//use the length property = how many items are in the array
+//dot syntax - fancy for use a period
+
+console.log(adventuresNames.length)
+
+//add an item in our array
+
+adventuresNames[4]="Haweye";
+adventuresNames[adventuresNames.length]="wolverine";
+
+console.log(adventuresNames);
+
+
+//Picking Oranges
+
+var orangesPicked =[13, 350, 1000]
+
+//How many oraganes did we pick in todal
+
+var totalOranges = orangesPicked[0]+ orangesPicked[1]+orangesPicked[2];
+
+console.log("The Total Number of Oranges Picked is "+totalOranges+ ".");
+
+//var averageOranges = totalOranges/orangesPicked.length;
+
+var averageOranges = totalOranges/3;
+console.log("The average number of oranges  picked per day is "+averageOranges+".");
+
