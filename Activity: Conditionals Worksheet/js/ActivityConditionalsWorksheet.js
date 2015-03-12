53fe992e4ba8989076f007862bc2e6d53e496c03
hufi94 <dj_hufi@me.com>
2015-03-12 13:48:36 -0400

    DAY 5 Stakes

---
+++ Activity: Conditionals Worksheet/js/ActivityConditionalsWorksheet.js
@@ -0,0 +1,31 @@
+/* Matthias Hufnagl
+ 3-7-15
+ section 00
+ Day 4 Activity: Conditionals Worksheet
+ */
+
+//alert("test to see if it werks");
+
+
+var farenheit = "f";
+var celsius ="c";
+var convertC = 9 / 5 + 32;
+var convertF =(convertAsk -32)*5/9;
+
+var convertAsk = prompt("Please enter\n C or Celsius or F for Fahrenheit");
+
+var tamp = prompt("Please enter how hot it is");
+
+
+
+
+
+
+
+if(convertAsk === celsius){
+    console.log("The temperature is "+finalC+" degrees Celsius");
+}else if (convertAsk === farenheit){
+    var finalF =farenheit * convertF;
+    console.log(finalF);
+
+}
---
+++ Day 5/js/day5stakes.js
@@ -0,0 +1,7 @@
+/* Matthias Hufnagl
+ 3-12-15
+ section 00
+ Day 5 stakes
+ */
+
+alert("test to see if it werks");
