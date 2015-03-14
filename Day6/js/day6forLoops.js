9b2b48956ed15b7dfaf262a29c650195e3c196a6
hufi94 <dj_hufi@me.com>
2015-03-14 16:58:58 -0400

    for loops and do loop san more

--- Day6/js/day6forLoops.js
+++ Day6/js/day6forLoops.js
@@ -4,4 +4,49 @@
  Day 6 for loops
  */
 
-alert("test to see if it werks");
+//alert("test to see if it werks");
+
+
+//for(initinalstion; condition; incroment of change) {}
+//we use i alot i loops it stand for iteration
+
+for(var i = 0; i<20; i++){
+
+    console.log("the value of i is "+i);
+}
+
+console.log(i);
+
+//Brak in a loop
+
+for(var j=0; j<5; j++){
+
+    console.log("the value of j is " +j);
+
+    if(j===3){
+        console.log("j is 3!!");
+        break;
+
+    }
+console.log("the vaule of j is "+j);
+}
+
+//(var j=5; j>0; j--)
+
+for(var k=0; k<3; k++){
+
+    if(k===0){
+        console.log("the value of zero ");
+
+
+    }else if(k===1){
+
+        console.log("the value is one")
+
+    }else if (k===2){
+
+        console.log("the value is two")
+    }
+
+
+}
