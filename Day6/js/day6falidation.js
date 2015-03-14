62f37e124a854a20c41b3b35c79b77a3cb16bfac
hufi94 <dj_hufi@me.com>
2015-03-14 17:27:30 -0400

    Vaildations
    Vaildations

--- Day6/js/day6falidation.js
+++ Day6/js/day6falidation.js
@@ -4,4 +4,43 @@
  Day 6 falidation
  */
 
-alert("test to see if it werks");
+//alert("test to see if it werks");
+
+//ask the use for a num so var
+
+var num1 = prompt("please enter a number!")
+
+
+//validate using a while
+
+while(isNaN(num1)|| num1 ===""){
+    //repropmt user
+
+    if(num1=== ""){
+        //the user left it blank
+        num1 = prompt("please don lot leve blak, please type in a number")
+
+    }else if(isNaN(num1)){
+
+        num1 = prompt("please only type in numbers.\nplease enter a number")
+    }
+
+
+
+
+}
+
+
+//ask the user a yes or no Q
+
+var userInput= prompt("please enter yes or no");
+//convert to lowercase
+//validate
+userInput = userInput.toLowerCase();
+
+while(userInput != "yes" && userInput !="no" && userInput !="nope"){
+
+    //reprompt
+    userInput = prompt("please only in yes or no");
+
+}
