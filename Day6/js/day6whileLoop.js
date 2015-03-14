d8d454222cebd01ae6115c270d58441cea03299c
hufi94 <dj_hufi@me.com>
2015-03-14 16:34:19 -0400

    while loops and do while loops
    while loops and do while loops

--- Day6/js/day6whileLoop.js
+++ Day6/js/day6whileLoop.js
@@ -5,3 +5,42 @@
  */
 
 //alert("test to see if it werks");
+
+
+
+//BASIC WHILE Loop
+
+//inessulise start a var
+
+var conter =0;
+
+//star while loop
+//condidion to test goes inside of ()
+while(conter<200){
+    console.log("never end");
+    console.log(conter);
+
+    conter++
+}
+//make a change to the conting var
+
+
+//do while loop
+//this loop will run the code fist thn check the condition
+
+/*
+do{
+code run
+} while (condition)to test
+
+ */
+
+var i = 20;
+
+do{
+    console.log("the num is "+i);
+    //change the conting var
+
+    i++;
+
+}while(i<10);
