20e243b28ac9a19512cba8aa5dfc6af650e7eb2d
hufi94 <dj_hufi@me.com>
2015-03-17 15:58:51 -0400

    scope

--- Day 5/js/day7scope.js
+++ Day 5/js/day7scope.js
@@ -6,4 +6,39 @@
 
 //alert("test to see if it werks");
 
-//
+//varible scope
+
+//varibles inside and outside of a function
+
+// try not to use the same varible names but in bug files its goig to be imposible
+
+//create a var for with in aour main code scoped outside of the function - main code
+
+var width=5;
+
+function calcPeri(){
+
+    //create a var width indide of th function
+    //this is scoped to functiom cal
+
+    var width =10;
+
+    // create a varible for height an d perimiter
+
+    var height =20;
+
+    var perimeter = width*2 + height*2;
+
+    console.log("inside of the function , the perimiter is "+perimeter);
+
+
+
+}
+
+console.log("before the function call, width ="+width);
+
+calcPeri();
+
+console.log("after the function call, width, the perimiter is "+width);
+
+
