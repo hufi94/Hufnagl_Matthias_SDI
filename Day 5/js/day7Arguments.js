d49cff51d10929ea64a968f07c08ba95235d895d
hufi94 <dj_hufi@me.com>
2015-03-17 16:30:23 -0400

    functions arguments

--- Day 5/js/day7Arguments.js
+++ Day 5/js/day7Arguments.js
@@ -4,4 +4,42 @@
  Day day 7 arguments
  */
 
-alert("test to see if it werks");
+//alert("test to see if it werks");
+
+//argiments - goes into the function parts of the functions callsed
+
+//add in perameters
+
+function calcArea(w,h){
+    //var width=10;
+    //var height=20;
+    var area = w*h;
+    console.log("The Area is "+area);
+}
+
+
+//add arguments to the function call
+calcArea(10,20);
+calcArea(20,40);
+
+var width = prompt("enter a width");
+
+var height =prompt("enter height");
+
+calcArea(width,height);
+
+//dog years
+
+function dogYears(humanAge){
+
+    var dogAge = humanAge * 7;
+
+    console.log("your age in dog years is "+dogAge)
+
+}
+
+dogYears(4)
+
+var userInput = prompt("perlase enter your human age")
+
+dogYears(userInput);
