861a9f704da8c86443705cee19c95cdfe79e6ab5
hufi94 <dj_hufi@me.com>
2015-03-17 15:25:14 -0400

    functions

---
+++ Day 5/js/day7functions.js
@@ -0,0 +1,43 @@
+/* Matthias Hufnagl
+ 3-17-15
+ section 00
+ Day 7 functions
+ */
+printHello();
+//alert("test to see if it werks");
+
+//function functionName(){
+//
+// }
+
+function printHello(){
+
+    console.log("Hello");
+
+}
+
+function printMore(){
+
+    console.log("Print more text")
+
+
+}
+
+printHello();
+printMore()
+printHello();
+
+printMore();
+
+// create a function that calcutates of a rectangle
+
+function calcArea(){
+
+    var width =20;
+    var height =30;
+    var area =width*height;
+
+    console.log("the area "+area);
+}
+
+calcArea();
