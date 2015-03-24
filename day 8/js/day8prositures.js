4ef784ca5918fb282277067fb5624b87b382c115
hufi94 <dj_hufi@me.com>
2015-03-24 15:09:39 -0400

    day 8 prositroes
    day 8 prositroes done

--- day 8/js/day8prositures.js
+++ day 8/js/day8prositures.js
@@ -6,4 +6,31 @@
 
 //alert("test to see if it werks");
 
+//this is a basic function
 
+function calcAreaF(w,h){
+
+    var area = w*h;
+    return area;
+
+
+}
+var area =calcAreaF(6,8);
+console.log("the area of the ractangle is "+area);
+
+// this is a prositure / ist of steps
+
+
+function calcAreaP(w,h){
+
+    var area = w*h;
+
+    // we do not return this area
+    console.log("Inside the prosidure of the area is "+area);
+
+
+}
+
+//just the func name no var to catch
+
+calcAreaP(12,16);
