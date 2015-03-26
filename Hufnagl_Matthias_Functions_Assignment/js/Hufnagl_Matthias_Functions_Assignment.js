7e1e365300fb5079f69b42f5906c3f87df680779
hufi94 <dj_hufi@me.com>
2015-03-26 13:23:55 -0400

    day 9 pizza

---
+++ Hufnagl_Matthias_Functions_Assignment/js/Hufnagl_Matthias_Functions_Assignment.js
@@ -0,0 +1,40 @@
+/* Matthias Hufnagl
+ 3-7-15
+ section 00
+ Day 8 Functions_Assignment
+ */
+
+//alert("test to see if it werks");
+
+
+
+
+
+//var askForHomework = prompt("What did you get on your homework");
+//var askForAssignment= prompt("What did you get on your Assignment");
+
+function grade1( quiz, homework, assignment) {
+
+    var quiz = prompt("what did you get on your quiz.");
+    return quiz;
+}
+
+function grade2(quiz, homework, assignment) {
+
+    var homework = prompt("what is your homework grade")
+
+    return homework;
+}
+
+function grade3(quiz, homework, assignment) {
+
+    var assignment = prompt("what is your assignment grade")
+
+    return assignment;
+}
+
+
+
+
+
+
---
+++ day 9/js/day9Pizza.js
@@ -0,0 +1,7 @@
+/* Matthias Hufnagl
+ 3-26-15
+ section 00
+ Day 9 Pizza
+ */
+
+//alert("test to see if it werks");
